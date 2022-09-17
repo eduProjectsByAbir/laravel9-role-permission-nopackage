68118a361595463b16c89460f2aa8d76a7ce7ca5
Abir <abiruzzaman.molla@gmail.com>
2022-09-17 15:30:25 +0600

    role and premission sidebar

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])
@@ -8,4 +10,6 @@ Route::middleware(['auth', 'role:admin'])
     ->name('admin.')
     ->group(function () {
         Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+        Route::resource('/roles', RoleController::class);
+        Route::resource('/premissions', PermissionController::class);
     });
