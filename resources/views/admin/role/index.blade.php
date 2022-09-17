54cf4f4a3e15ab093a959a0bee814ec2cb4f2bcb
Abir <abiruzzaman.molla@gmail.com>
2022-09-17 16:03:51 +0600

    filter by deign

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -1,13 +1,33 @@
 @extends('admin.layouts.admin-layout')
 @section('body')
 <div class="flex mb-4">
-    <div class="w-1/2">
+    <div class="w-1/3">
         <h3 class="text-gray-700 text-3xl font-medium">All Roles</h3>
     </div>
-    <div class="w-1/2 text-right">
+    <div class="w-1/3">
+        <h3 class="text-gray-700 text-3xl font-medium">
+            <div class="px-3 mb-6 md:mb-0">
+                <div class="relative">
+                    <select
+                        class="block appearance-none w-full bg-gray-200 border border-blue-500 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-state">
+                        <option>Filter By</option>
+                        <option>New Mexico</option>
+                        <option>Missouri</option>
+                        <option>Texas</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                            </svg>
+                    </div>
+                </div>
+            </div>
+        </h3>
+    </div>
+    <div class="w-1/3 text-right">
         <a href="#" class="text-indigo-600 hover:text-indigo-900">
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                 Create New
             </button>
         </a>
